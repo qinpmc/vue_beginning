67f7f3f79e9c011802b10b313dae409ff3541c34
qinpmc <850823973@qq.com>
2019-03-15 17:11:01 +0800

    add vuex

---
+++ vuex/noclivuex/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr>
+        <h3>{{$store.state.count}}</h3>
+        <h3>{{count}}</h3>
+    </div>
+    <div>
+        <button @click="$store.commit('add')">+</button>
+        <button @click="$store.commit('reduce')">-</button>
+    </div>
+</div>
+</template>
+
+
+
+<script>
+import store from './store.js'
+import {mapState} from 'vuex';
+
+export default {
+        // computed:{
+        //     count(){
+        //         return this.$store.state.count;
+        //     }
+        // },
+
+
+        // computed:mapState({
+        //     count:state => state.count
+        // }),
+
+        computed:mapState(["count"]),
+
+        data(){
+            return {
+                msg:'hello vuex'
+            }
+        },
+        //store:store
+}
+</script>
+
+ 
---
+++ vuex/vuex-jishupang/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr>
+        <h3>{{$store.state.count}}</h3>
+    </div>
+    <div>
+        <button @click="$store.commit('add')">+</button>
+        <button @click="$store.commit('reduce')">-</button>
+    </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+
+        data(){
+            return {
+                msg:'hello vuex'
+            }
+        }
+}
+</script>
