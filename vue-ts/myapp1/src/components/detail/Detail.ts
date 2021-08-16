6643e3ed179564edbb40cc41258f06bd9ce7b6a5
qinpmc <850823973@qq.com>
2021-08-16 17:32:06 +0800

    删除分支上多余tss

--- vue-ts/myapp1/src/components/detail/Detail.ts
+++
@@ -1,8 +0,0 @@
-
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class Detail extends Vue {
-    desc: any = "ceshi"
- 
-}
--- vue-ts/myapp1/src/views/Info.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h2>{{title}}</h2>
-    <Detail />
-  </div>
-</template>
-
-<script lang="ts" src="./Info.ts"></script>
